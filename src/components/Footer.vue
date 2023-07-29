6466e91f06bb3e9e8d6b437e7b5f40ab3ca4341f
wezzganteng <fawwazalfaroz@gmail.com>
2023-07-29 14:05:42 +0700

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
-                <img src=https://www.pngarts.com/files/2/Letter-F-Download-PNG-Image.png" class="h-8 mr-3" alt="Flowbite Logo" />
+                <img src="https://www.pngarts.com/files/2/Letter-F-Download-PNG-Image.png" class="h-8 mr-3" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FAWWAZ</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
